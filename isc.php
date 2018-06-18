b0e86427e1f1bb84f5317cec76f021629e0c829d
Givi Odikadze <givi.odikadze@grassvalley.com>
2018-06-17 21:45:11 -0400

    added isc file

---
+++ isc.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Library\Isc\Console\Modules\IscClientRunner;
+
+$app = new Application();
+
+$env = new \Library\Core\Environment(__DIR__);
+$env->load();
+
+$app->add(new IscClientRunner(__DIR__));
+
+$app->run();
